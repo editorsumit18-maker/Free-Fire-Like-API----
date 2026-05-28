627e062c962db64b102960b31a73a16e49373b78
editorsumit18-maker <editorsumit18@gmail.com>
2026-05-28 08:19:17 +0530

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 import requests
 import json
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run()
 
 UIDPASS_FILE = "uidpass.json"
 TOKEN_FILE = "tokens.json"
